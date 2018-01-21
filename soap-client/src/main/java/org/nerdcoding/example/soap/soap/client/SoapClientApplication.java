3e3f19be8ba85844b7d016a89290d162ec17737d
Tobias Koltsch <tobias@koltsch.de>
2018-01-21 10:49:53 +0100

    Call the WeatherEndpoint from the `soap-server.
    With the `jaxws-maven-plugin` the JDKs wsimport is called and the web
    service stubs are generated. A simple REST endpoint calls the SOAP web
    service and delivers the result.

--- soap-client/src/main/java/org/nerdcoding/example/soap/soap/client/SoapClientApplication.java
+++ soap-client/src/main/java/org/nerdcoding/example/soap/soap/client/SoapClientApplication.java
@@ -1,12 +1,20 @@
 package org.nerdcoding.example.soap.soap.client;
 
+import org.nerdcoding.example.soap.server.web.WeatherEndpoint;
+import org.nerdcoding.example.soap.server.web.WeatherEndpointService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoapClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoapClientApplication.class, args);
+	}
+
+	@Bean
+	public WeatherEndpoint weatherEndpoint() {
+		return new WeatherEndpointService().getWeatherEndpointPort();
 	}
 }
---
+++ soap-client/src/main/java/org/nerdcoding/example/soap/soap/client/web/ClientRestController.java
@@ -0,0 +1,47 @@
+/*
+ * ClientRestController.java
+ *
+ * Copyright (c) 2018, Tobias Koltsch. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
+ */
+
+package org.nerdcoding.example.soap.soap.client.web;
+
+import org.nerdcoding.example.soap.server.web.Weather;
+import org.nerdcoding.example.soap.server.web.WeatherEndpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller provides a REST endpoint used to call the `sopa-client`.
+ */
+@RestController
+@RequestMapping("/client/api/rest")
+public class ClientRestController {
+
+    private final WeatherEndpoint weatherEndpoint;
+
+    @Autowired
+    public ClientRestController(final WeatherEndpoint weatherEndpoint) {
+        this.weatherEndpoint = weatherEndpoint;
+    }
+
+    @GetMapping("/weather")
+    public Weather callSoapWebService() {
+        return weatherEndpoint.getWeather();
+    }
+
+}
